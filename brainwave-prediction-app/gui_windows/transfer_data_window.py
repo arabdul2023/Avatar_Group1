fd4c251131bd1f4c9017d9e8d0c0485950079dc5
Leone-Daniel <dleone@stcloudstate.edu>
2023-10-08 21:20:35 -0500

    Window layout has been modified. The user will either type or use the browse function to find a directory. A Transfer button has been added that currently displays a message indicating if the directory path is valid or not. This button will be linked to the file transfer script.

--- brainwave-prediction-app/gui_windows/transfer_data_window.py
+++ brainwave-prediction-app/gui_windows/transfer_data_window.py
@@ -1,36 +1,55 @@
 import PySimpleGUI as sg
 import sys
+import os
 
 def transfer_data_window(items, get_drone_action, window1):
 
+
+    def is_valid_directory(input_value):
+        # Check if the input is a valid directory path
+        return os.path.isdir(input_value)
+    
     # Layout
-    header_font_size = 20
-    header = [
-        [sg.Text("Transfer Data Screen", font=('Arial', header_font_size), justification='c', auto_size_text=True)],
-    ]    
+    header_font_size = 35
+    data_transfer_message_size = 13
+    top_left = [
+        [sg.Button('Home', size=(8, 2), image_filename="../brainwave-prediction-app/images/home.png")]     
+    ]
+    
+    top_center = [
+        [sg.Text("Transfer Data Screen", font=('Arial', header_font_size), justification='c', auto_size_text=True)]    
+    ]
+    
+    data_transfer = [
+        [sg.Text("Select a locally stored directory for transfer...", font=('Arial', data_transfer_message_size))],
+        [sg.Text('Folder Path:'), sg.Input(key='-dir_input-'), sg.FolderBrowse(target='-dir_input-')],
+    ]
 
     transfer_data_layout = [
-        [sg.Button('Home', size=(8, 2), image_filename="../brainwave-prediction-app/images/home.png"), sg.Push()], 
-        [sg.Column(header, pad=((0, 55), (0, 0)))],
-        [sg.Text("Locally stored directory")],
-        [sg.Button('File Path'), sg.Input(key='-file_input-')],
-        [sg.Button("OK", size=(10,1))], 
+        [sg.Column(top_left), sg.Push(), sg.Column(top_center, pad=((0, 175), 0)), sg.Push(), ],
+        [sg.Column(data_transfer, pad=(0, (125, 25)))],
+        [sg.Button("Transfer", size=(10,1))]        
     ]    
         
     transfer_data_window = sg.Window("Transfer Data", transfer_data_layout, size=(
-        1200, 800), element_justification='c', finalize=True)
-    
+        1200, 800), element_justification='c', finalize=True) 
     
     while True:
         event, values = transfer_data_window.read()
             
         if event == sg.WIN_CLOSED:
             sys.exit()
-            break
         elif event == "Home":
             break
-        elif event == "OK":
-            sg.popup("Button OK was clicked!")
-            
+        elif event == "Transfer":
+            #sg.popup("Button OK was clicked!")
+            selected_directory = values['-dir_input-']
+            if is_valid_directory(selected_directory):
+                sg.popup(f"Selected folder: {selected_directory}")
+            else:
+                sg.popup("Invalid directory. Please select a valid directory.")
+                # Clear the invalid input
+                transfer_data_window['-dir_input-'].update('')
+ 
     window1.un_hide()
     transfer_data_window.close()
