868166e67eb36ea00540af0c823c120a12cf4a55
Leone-Daniel <dleone@stcloudstate.edu>
2023-10-13 23:47:20 -0500

    I have cleaned up the window. There is no real functionality to the buttons yet. Some of the original behavior needs reworking.

--- brainwave-prediction-app/gui_windows/transfer_data_window.py
+++ brainwave-prediction-app/gui_windows/transfer_data_window.py
@@ -10,35 +10,45 @@ def transfer_data_window(window1):
     
     # Positions items in top left corner of the window
     top_left = [
-        [sg.Button('Home', size=(8, 2), image_filename="../brainwave-prediction-app/images/home.png")]     
+    #    [sg.Button('Home', size=(8, 2), image_filename="../brainwave-prediction-app/images/home.png")]     
     ]
 
     # Positions items in the top center of the window
     title_font_size = 35   # adjust title font size
-    top_center = [
-        [sg.Text("Transfer Data Screen", font=('Arial', title_font_size), justification='c', auto_size_text=True)]    
+    header = [
+        [sg.Text("Transfer Data Screen", font=('Arial', title_font_size), justification='c', pad=((0,0),(100,50)), auto_size_text=True)]    
     ]
-
-    user_data = [
-        [sg.Text('Host:\t   '), sg.Input(key='-dir_input-')],
-        [sg.Text('User Name:'), sg.Input(key='-dir_input-')],
-        [sg.Text('Private Key:'), sg.Input(key='-dir_input-')],
-        [sg.Text('Password:  '), sg.Input(key='-dir_input-')],
-    ]    
 
     # Contains items for directory input of user in the window
     directory_input_message_size = 13   # adjut directoy input message size
     directory_input = [
         [sg.Text("Select a directory for transfer...", font=('Arial', directory_input_message_size))],
-        [sg.Text('Folder Path:'), sg.Input(key='-dir_input-'), sg.FolderBrowse(target='-dir_input-')],
+        [sg.Text('Folder Path:'), sg.Input('', key='-dir_input-')], 
+        [sg.FolderBrowse(target='-dir_input-', size=(10,1), pad=(5,5)), 
+            sg.Button('Open', size=(10,1), pad=(5, 5)), 
+            sg.Button('Clear',size=(10,1))],
     ]
 
+    user_data = [
+        [sg.Text('Host:\t   '), sg.Input('', key='-dir_input-')],
+        [sg.Text('User Name:'), sg.Input('', key='-dir_input-')],
+        [sg.Text('Private Key:'), sg.Input('', key='-password-', password_char='*')],
+        [sg.Text('Password:  '), sg.Input('', key='-password-', password_char='*')],
+        [sg.Button('OK', size=(10,1), pad=((5, 5))), sg.Button('Clear',size=(10,1))],    
+    ]
+    
+    finalize_buttons = [
+        [sg.Button("Transfer", size=(30,3), pad=((0, 50), (0,0))), sg.Button("Cancel  ", size=(30,3))]    # Button for executing transfer           
+    ]
+
+    directory_input_frame = sg.Frame('Open Directory', directory_input, pad=((0,0) , (0,50)))
+    user_data_frame = sg.Frame('Log In', user_data, pad=((0,0) , (0,50)))
     # Combined window layout
     transfer_data_layout = [
-        [sg.Column(top_left), sg.Push(), sg.Column(top_center, pad=((0, 175), 0)), sg.Push(), ],
-        [sg.Column(user_data, pad=(0, (25, 25)))],
-        [sg.Column(directory_input, pad=(0, (25, 25)))],
-        [sg.Button("Transfer", size=(10,1))]    # Button for executing transfer       
+        [header],
+        [directory_input_frame],
+        [user_data_frame],
+        [finalize_buttons]
     ]    
     
     # Window
