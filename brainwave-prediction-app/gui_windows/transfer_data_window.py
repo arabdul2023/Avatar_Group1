887e6abcba8474ec22eb349f9901a482608748be
Leone-Daniel <dleone@stcloudstate.edu>
2023-10-14 11:16:06 -0500

    GUI has been reworked. Clear buttons clear appropriate fields. Cancel button sends user back to the home page. Some buttons are disabled. 'sftp' file opens are the beginning of window opened.

--- brainwave-prediction-app/gui_windows/transfer_data_window.py
+++ brainwave-prediction-app/gui_windows/transfer_data_window.py
@@ -4,6 +4,12 @@ import os
 
 def transfer_data_window(window1):
 
+    file_path = "../file-transfer/sftp.py"
+    if file_path:
+        with open(file_path, 'r') as file:
+            file_contents = file.read()
+            #sg.popup(f'File Contents:\n{file_contents}' )
+
     def is_valid_directory(input_value):
         # Check if the input is a valid directory path
         return os.path.isdir(input_value)
@@ -19,35 +25,35 @@ def transfer_data_window(window1):
         [sg.Text("Transfer Data Screen", font=('Arial', title_font_size), justification='c', pad=((0,0),(100,50)), auto_size_text=True)]    
     ]
 
+    login_data = [
+        [sg.Text('Host:\t   '), sg.Input('', key='-host_input-')],
+        [sg.Text('User Name:'), sg.Input('', key='-name_input-')],
+        [sg.Text('Private Key:'), sg.Input('', key='-key_input-', password_char='*')],
+        [sg.Text('Password:  '), sg.Input('', key='-pass_input-', password_char='*')],
+        [sg.Button('OK', size=(10,1), pad=((5, 5)), disabled=True,), sg.Button('Clear', key='Clear_Login_Data', size=(10,1))],    
+    ]
+
     # Contains items for directory input of user in the window
     directory_input_message_size = 13   # adjut directoy input message size
     directory_input = [
         [sg.Text("Select a directory for transfer...", font=('Arial', directory_input_message_size))],
         [sg.Text('Folder Path:'), sg.Input('', key='-dir_input-')], 
         [sg.FolderBrowse(target='-dir_input-', size=(10,1), pad=(5,5)), 
-            sg.Button('Open', size=(10,1), pad=(5, 5)), 
-            sg.Button('Clear',size=(10,1))],
-    ]
-
-    user_data = [
-        [sg.Text('Host:\t   '), sg.Input('', key='-dir_input-')],
-        [sg.Text('User Name:'), sg.Input('', key='-dir_input-')],
-        [sg.Text('Private Key:'), sg.Input('', key='-password-', password_char='*')],
-        [sg.Text('Password:  '), sg.Input('', key='-password-', password_char='*')],
-        [sg.Button('OK', size=(10,1), pad=((5, 5))), sg.Button('Clear',size=(10,1))],    
+            sg.Button('Open', size=(10,1), pad=(5, 5), disabled=True), 
+            sg.Button('Clear', key='Clear_Directory', size=(10,1))],
     ]
     
     finalize_buttons = [
-        [sg.Button("Transfer", size=(30,3), pad=((0, 50), (0,0))), sg.Button("Cancel  ", size=(30,3))]    # Button for executing transfer           
+        [sg.Button("Transfer", size=(30,3), pad=((0, 50), (0,0)), disabled= True), sg.Button("Cancel", size=(30,3))]    # Button for executing transfer           
     ]
 
+    login_data_frame = sg.Frame('Log In', login_data, pad=((0,0) , (0,50)))
     directory_input_frame = sg.Frame('Open Directory', directory_input, pad=((0,0) , (0,50)))
-    user_data_frame = sg.Frame('Log In', user_data, pad=((0,0) , (0,50)))
     # Combined window layout
     transfer_data_layout = [
         [header],
+        [login_data_frame],
         [directory_input_frame],
-        [user_data_frame],
         [finalize_buttons]
     ]    
     
@@ -61,14 +67,23 @@ def transfer_data_window(window1):
             
         if event == sg.WIN_CLOSED:    # completely close program if 'x' is clicked
             sys.exit()
-        elif event == "Home":   # return to start screen
+        elif event == "Cancel":   # return to start screen
             break
-        elif event == "Transfer":   # check if valid directory and transfer
+        elif event == 'Clear_Login_Data':
+            transfer_data_window['-host_input-'].update('')
+            transfer_data_window['-name_input-'].update('')
+            transfer_data_window['-key_input-'].update('')
+            transfer_data_window['-pass_input-'].update('')
+            sg.popup('Login Data Cleared')
+        elif event == 'Clear_Directory':
+            transfer_data_window['-dir_input-'].update('')
+            sg.popup('Directory Cleared')
+        elif event == 'Transfer':
             selected_directory = values['-dir_input-']
             if is_valid_directory(selected_directory):
-                sg.popup(f"Selected folder: {selected_directory}")
+                sg.popup(f'Selected folder: {selected_directory}')
             else:
-                sg.popup("Invalid directory. Please select a valid directory.")
+                sg.popup('Invalid directory. Please select a valid directory.')
                 transfer_data_window['-dir_input-'].update('')
 
     window1.un_hide()
